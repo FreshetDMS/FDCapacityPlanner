d4bb11ba17387914d39a53e2d094fff08c09eb95
Milinda Pathirage <milinda.pathirage@gmail.com>
2017-04-21 16:14:54 -0400

    changed y label of plot

--- results/plots/overall-tpt-reduction.r
+++ results/plots/overall-tpt-reduction.r
@@ -26,7 +26,7 @@ tikz(file = output_file, width = 3.3, height = 2.2)
 iops_data <- read.csv(input_csv)
 iops_data$logs <- with(iops_data, leaders + followers)
 
-plot <- ggplot(iops_data, aes(x=logs, y=iops)) + geom_point() + geom_smooth(method = "auto") + scale_y_continuous(name="Disk Throughput (IOPS)") + scale_x_continuous(name="Number of Files") + theme(axis.line = element_line(colour = "black"), panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.border = element_blank(),panel.background = element_blank()) + expand_limits(x = 1, y = 400)
+plot <- ggplot(iops_data, aes(x=logs, y=iops)) + geom_point() + geom_smooth(method = "auto") + scale_y_continuous(name="Throughput (IOPS)") + scale_x_continuous(name="Number of Files") + theme(axis.line = element_line(colour = "black"), panel.grid.major = element_blank(), panel.grid.minor = element_blank(),panel.border = element_blank(),panel.background = element_blank()) + expand_limits(x = 1, y = 400)
 
 # legend.position="top", legend.box = "horizontal",
 #This line is only necessary if you want to preview the plot right after compiling
